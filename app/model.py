f627272da4752820b6d48764bc5aefd5d0916002
ekemini <ekemini@leytro.com>
2023-06-04 23:36:37 +0100

    Code

--- app/model.py
+++ app/model.py
@@ -3,11 +3,11 @@ import pickle
 import sklearn
 from pydantic import BaseModel
 
-__version__ = "0.10"
+__version__ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
-with open(f"{BASE_DIR}/best_gs_pipeline_{__version__}.pkl", "rb") as f:
+with open(f"{BASE_DIR}/best_gs_model_{__version__}.pkl", "rb") as f:
     model = pickle.load(f)
 
 class DataIn(BaseModel):
