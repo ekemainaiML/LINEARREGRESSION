efa04e1b49cb4ec34a68df0ce5666d6abc8117a4
ekemini <ekemini@leytro.com>
2023-06-04 15:12:30 +0100

    Remove unnecessary files

--- normalizedata.m
+++
@@ -1,5 +0,0 @@
-function N = normalizedata(X)
- m = mean(X);
- s = std(X);
- N = abs((X - m) ./ s);
-end
--- scatterPlot.m
+++
@@ -1,7 +0,0 @@
-function aa = scatterPlot(xa, y)
-  figure
-  aa = plot(xa, y);
-  xlabel ("x");
-  ylabel ("y");
-  title ("Synchronous Motor");
-end
